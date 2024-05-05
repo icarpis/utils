8ca406f482d825f31fe6e63cc48d36b53b02a682
Itay Carpis <itay.carpis@gmail.com>
2024-05-05 09:16:12 +0300

    Update main.cpp for Peterson's algo use

--- main.cpp
+++ main.cpp
@@ -1,45 +1,123 @@
 #include "custom_allocator.h"
 #include "mem_pool.h"
-
+#include <cstdio>
+#include <cstdlib>
+#include <thread>
+#include <mutex>
+#include "peterson's_algo_for_n_process.h"
 
 
 #define SIZE (1024*1024)
 uint8_t lmem[SIZE] = { 0 };
 
 
-int main()
+
+std::mutex mtx;
+
+void cpu0()
 {
-    while (1)
+    enter_critical_section(0);
+    int x = 1;
+    x++;
+
+    // critical section
     {
+        std::cout << "P0\n";
+        std::unique_lock<std::mutex> lock(mtx, std::defer_lock);
         {
-            memory_pool<> mp(SIZE);
-            void* ptr = mp.malloc(111, 1024);
-            mp.free(ptr);
-        }
-
-        {
-            memory_pool<custom_allocator<uint8_t>> mp(SIZE);
-            void* ptr = mp.malloc(SIZE / 4);
-            mp.free(ptr);
-        }
-
-        {
-            memory_pool<custom_allocator<uint8_t>> mp(SIZE);
-            void* ptr = mp.malloc(SIZE);
-            mp.free(ptr);
-        }
-
-        {
-            memory_pool<custom_allocator<uint8_t>> mp(SIZE);
-            void* ptr = mp.malloc(10, SIZE);
-            mp.free(ptr);
-        }
-
-        {
-            memory_pool<custom_allocator<uint8_t>> mp(SIZE);
-            void* ptr = mp.malloc(1024, 128);
-            mp.free(ptr);
+            if (!lock.try_lock())
+            {
+                std::cout << "Race Condition!!!!!!!!!!";
+            }
         }
     }
+
+    leave_critical_section(0);
+}
+
+void cpu1()
+{
+    enter_critical_section(1);
+    int x = 1;
+    x++;
+
+    // critical section
+    {
+        std::cout << "P1\n";
+        std::unique_lock<std::mutex> lock(mtx, std::defer_lock);
+        {
+            if (!lock.try_lock())
+            {
+                std::cout << "Race Condition!!!!!!!!!!";
+            }
+        }
+    }
+
+    leave_critical_section(1);
+}
+
+void cpu2()
+{
+    enter_critical_section(2);
+    int x = 1;
+    x++;
+
+    // critical section
+    {
+        std::cout << "P2\n";
+        std::unique_lock<std::mutex> lock(mtx, std::defer_lock);
+        {
+            if (!lock.try_lock())
+            {
+                std::cout << "Race Condition!!!!!!!!!!";
+            }
+        }
+    }
+
+    leave_critical_section(2);
+}
+
+
+int main()
+{
+    // memory_pool
+    {
+        memory_pool<> mp(SIZE);
+        void* ptr = mp.malloc(111, 1024);
+        mp.free(ptr);
+    }
+
+    {
+        memory_pool<custom_allocator<uint8_t>> mp(SIZE);
+        void* ptr = mp.malloc(SIZE / 4);
+        mp.free(ptr);
+    }
+
+    {
+        memory_pool<custom_allocator<uint8_t>> mp(SIZE);
+        void* ptr = mp.malloc(SIZE);
+        mp.free(ptr);
+    }
+
+    {
+        memory_pool<custom_allocator<uint8_t>> mp(SIZE);
+        void* ptr = mp.malloc(10, SIZE);
+        mp.free(ptr);
+    }
+
+    {
+        memory_pool<custom_allocator<uint8_t>> mp(SIZE);
+        void* ptr = mp.malloc(1024, 128);
+        mp.free(ptr);
+    }
+
+
+    // Peterson's algo for n process
+    std::thread t11([]() { while (1) { cpu0(); } });
+    std::thread t22([]() { while (1) { cpu1(); } });
+    std::thread t33([]() { while (1) { cpu2(); } });
+    
+    t11.join();
+    
     return 0;
 }
